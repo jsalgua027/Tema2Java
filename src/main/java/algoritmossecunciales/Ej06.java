0bbf7b1c13d4bba79a6754643996d397cd42fa03
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2022-09-25 19:22:13 +0200

    terminado ejercicio 6

--- src/main/java/algoritmossecunciales/Ej06.java
+++ src/main/java/algoritmossecunciales/Ej06.java
@@ -22,11 +22,52 @@ public class Ej06 {
      */
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        double cantidadSocio1;
+        double cantidadSocio1;// variables para cada socio
         double cantidadSocio2;
-        double cantodadSocio3;
-        double totalInvertido;
-        final int MULTIPLICADOR=100;
+        double cantidadSocio3;
+        
+        double totalInvertido;// total de lo invertido entre los tres
+        
+        final int MULTIPLICADOR=100;// constante para el proceso arimetico
+        
+        double porcentajeSocio1;//variables resultado para cada socio
+        double porcentajeSocio2;
+        double porcentajeSocio3;
+        
+        // solicito los datos
+        System.out.println("Vamos a indicar la cantidad que invierte cada socio");
+        System.out.println("El socio UNO, ¿Que cantidad invierte?");
+        cantidadSocio1=teclado.nextDouble();
+        System.out.println("El socio DOS, ¿Que cantidad invierte?");
+        cantidadSocio2=teclado.nextDouble();
+        System.out.println("El socio TRES, ¿Que cantidad invierte?");
+        cantidadSocio3=teclado.nextDouble();
+        totalInvertido=cantidadSocio1+cantidadSocio2+cantidadSocio3;
+        System.out.println("El total de lo invertido por los tres socios es de: "+totalInvertido);
+        //proceso
+        porcentajeSocio1= (MULTIPLICADOR*cantidadSocio1)/totalInvertido;
+        porcentajeSocio2= (MULTIPLICADOR*cantidadSocio2)/totalInvertido;
+        porcentajeSocio3= (MULTIPLICADOR*cantidadSocio3)/totalInvertido;
+        
+        //muestro los resultados
+        System.out.println("*************************INFORME DE RESULTADOS*******************************");
+        
+        String resultados ="""
+                           Siendo el total de lo invertido %.2f 
+                           
+                            -El Socio UNO a invertido= %.2f que es: %.2f por ciento del total de la inversion
+                           
+                            -El Socio DOS a invertido= %.2f que es: %.2f por ciento del total de la inversion
+                           
+                            -El Socio TRES a invertido= %.2f que es: %.2f por ciento del total de la inversion
+                           
+                           
+                           
+                           """.formatted(totalInvertido,cantidadSocio1,porcentajeSocio1,
+                                   cantidadSocio2,porcentajeSocio2,cantidadSocio3,porcentajeSocio3);
+        
+        System.out.println(resultados);
+        
         
     }
     
