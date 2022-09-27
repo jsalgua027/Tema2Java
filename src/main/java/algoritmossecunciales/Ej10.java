8db4da4ffb693260e54e014f0eaf03369f65dfd5
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2022-09-27 22:30:38 +0200

    terminados ejercicio 10 y 11

---
+++ src/main/java/algoritmossecunciales/Ej10.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package algoritmossecunciales;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Windows10
+ */
+public class Ej10 {
+
+    /**
+     * Una empresa constructora se dedica a realizar bloques de pisos en
+     * parcelas como las que figuran en la imagen de la derecha. Necesitan saber
+     * el área de la parcela según las medidas A, B y C. Realiza el ejercicio
+     * usando JOption para solicitar y mostrar datos al usuario.
+     *
+     *
+     */
+    public static void main(String[] args) {
+        /*Solicito los datos mediante JOptionpane, uso la clase envolvente para
+        recoger un String y tranformalo a un double 
+         */
+        JOptionPane.showMessageDialog(null, "Le vamos a solicitar los lados de la figura");
+        double ladoA = Double.parseDouble(JOptionPane.showInputDialog("¿Cuanto mide A?"));
+        double ladoB = Double.parseDouble(JOptionPane.showInputDialog("¿Cuanto mide B?"));
+        double ladoC = Double.parseDouble(JOptionPane.showInputDialog("¿Cuanto mide C?"));
+
+        /*
+            realizo  los cálculos, para ello he dividido la figura en tres 
+            triangulos rectangulos para calcular el area de los dos primeros N y X
+            me basta con las dimensiones del ladoB y ladoC
+            para calcular el area del a necesito restar al ladoA el ladoC para sacar la altura.
+            y obtener el area F
+            en los tres triangulos uso la formula base por altura /2,
+            Despues sumo las tres areas para obtener el resultado final. 
+         */
+        // variables resultado de cada area
+        double areaN = (ladoC * ladoB) / 2;
+        double areaX = (ladoB * ladoC) / 2;
+        double areaF = ((ladoA - ladoC) * ladoB) / 2;
+        System.out.println(areaN + "-------" + areaX + "-----" + areaF);
+        double areaTotal = areaN + areaF + areaX;
+
+        //muestro el resultado
+        JOptionPane.showMessageDialog(null, "El area total es de: " + areaTotal);
+
+    }
+
+}
---
+++ src/main/java/algoritmossecunciales/Ej11.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package algoritmossecunciales;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Windows10
+ */
+public class Ej11 {
+
+    /**
+     * Se requiere obtener el área de la figura, a partir de R y H. Realiza un
+     * algoritmo para resolver el problema.
+     *
+     *
+     * El área de un semicírculo es la mitad del área de un círculo. Por lo
+     * tanto, el área de un semicírculo es igual al número pi por el cuadrado
+     * del radio del semicírculo partido por dos.
+     *
+     * La otra parte de la figura es un triangulo equilatero (Los tres lados
+     * iguales) usamos pitagoras para obtener la altira dado que transformamos
+     * el tringulo equilatero en dos triangulos rectangulos, para hayar la
+     * altura usamos pitagoras: c al cuadrado = la suma de los catetos al
+     * cuadrado la mitad de un lado cateto1 el lado entero el cateto2 una vez
+     * tengamos la altura = (base * altura/2)
+     *
+     *
+     *
+     */
+    public static void main(String[] args) {
+        double r=0;// siendo r radio
+        double h=0;// siendo h un lado del triangulo
+        
+        Scanner datos = new Scanner(System.in);
+        
+        System.out.println("Indique que radio tiene el semi circulo ");
+        r=datos.nextDouble();
+        System.out.println("Indique que dimension tiene uno de los lados del triangulo");
+        h=datos.nextDouble();
+        //cálculo el area del semi circulo
+        double areaSemiCirculo=(Math.PI*Math.pow(r, 2))/2;
+        //Cálculo el area del tringulo equilatero
+        double catetoTrinaguloRect=h/2;// variable para realizar pitagoras
+        double alturaTrinaguloEquila= Math.sqrt(Math.pow(h, 2)- Math.pow(catetoTrinaguloRect, 2));//obtengo la altura
+        double areaTrianguloEquila=(h*alturaTrinaguloEquila)/2;
+        //obtengo el resultado definitivo
+        double areaTotalFigura= areaSemiCirculo+areaTrianguloEquila;
+        
+        System.out.println("El area de la figura es= " + areaTotalFigura);
+        
+    }
+
+}
